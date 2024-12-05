e4a1c80c6e9d74a8afee7cbe629904bc5ea414c6
Dieter Best <dieterbest@gmail.com>
2024-12-05 11:22:15 -0800

    update snpEff database, add new reference genome for CBaratii: GCA_000789395.1_ASM78939v1

--- snpEff_database/make_snpEff_database.sh
+++ snpEff_database/make_snpEff_database.sh
@@ -2,13 +2,21 @@
 shopt -s expand_aliases
 alias snpEff="java -jar $HOME/Software/snpEff/snpEff.jar"
 
-ORG=NZ_CP014204.2
 
-mkdir -p data/$ORG
-cp ../data/$ORG.gb data/$ORG/genes.gbk
 cp $HOME/Software/snpEff/snpEff.config .
+
+ORG=NZ_CP014204.2
+mkdir -p data/$ORG
+cp ../data/C_baratii/reference/$ORG.gb data/$ORG/genes.gbk
 echo "${ORG}.genome: ${ORG}" >> snpEff.config
 snpEff build -c snpEff.config -genbank -v $ORG
+
+ORG=GCA_000789395.1_ASM78939v1_genomic
+mkdir -p data/$ORG
+cp ../data/C_baratii/reference/$ORG.gb data/$ORG/genes.gbk
+echo "${ORG}.genome: ${ORG}" >> snpEff.config
+snpEff build -c snpEff.config -genbank -v $ORG
+
 zip -r snpEff.zip data
 
 exit 0
