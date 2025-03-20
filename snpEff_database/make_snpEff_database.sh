321975e837c6d4b419fa5d568b0606246f281263
Dieter Best <dieterbest@gmail.com>
2025-03-20 16:13:32 -0700

    snpEff: add S_aureus to database

--- snpEff_database/make_snpEff_database.sh
+++ snpEff_database/make_snpEff_database.sh
@@ -2,22 +2,13 @@
 shopt -s expand_aliases
 alias snpEff="java -jar $HOME/Software/snpEff/snpEff.jar"
 
+ORG=CP000253.1
 
+mkdir -p data/$ORG
+cp $HOME/Analysis/nanopore/data/S_aureus/reference/$ORG.gb data/$ORG/genes.gbk
 cp $HOME/Software/snpEff/snpEff.config .
-
-ORG=NZ_CP014204.2
-mkdir -p data/$ORG
-cp ../data/C_baratii/reference/$ORG.gb data/$ORG/genes.gbk
 echo "${ORG}.genome: ${ORG}" >> snpEff.config
 snpEff build -c snpEff.config -genbank -v $ORG
-
-ORG=CP006905.1
-ORGFILE=../data/C_baratii/reference/GCA_000789395.1_ASM78939v1_genomic.gb
-mkdir -p data/$ORG
-cp $ORGFILE data/$ORG/genes.gbk
-echo "${ORG}.genome: ${ORG}" >> snpEff.config
-snpEff build -c snpEff.config -genbank -v $ORG
-
 zip -r snpEff.zip data
 
 exit 0
